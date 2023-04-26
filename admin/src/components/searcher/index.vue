ad7d5d4778e32daa424c30986f3d8707bca0dd13
Tenko_ <admin@tenko.life>
2023-04-26 16:37:32 +0800

    feat: 新增基础搜索器

--- admin/src/components/searcher/index.vue
+++ admin/src/components/searcher/index.vue
@@ -1,13 +1,79 @@
 <template>
-$END$
+    <el-select
+            :multiple="multiple ?? false"
+            :disable="disable"
+            :size="size ?? 'default'"
+            filterable
+            remote
+            v-model="value"
+            :remote-method="loadSelect"
+            :placeholder="placeholder!"
+    >
+        <el-option
+                v-for="item in lists"
+                :key="item[optionKey]"
+                :label="parseLabel(item)"
+                :value="item[optionKey]"
+        />
+    </el-select>
 </template>
 
-<script>
-export default {
-name: "index"
+<script setup lang="ts" name="BaseSearcher">
+type KeywordType = string | number | string[] | number[]
+type InjectProps = Record<string, any>
+type SearchProps = InjectProps & { [key: string]: KeywordType }
+
+interface Props {
+    modelValue: KeywordType
+    optionKey?: string
+    keyword?: string
+    multiple?: boolean
+    disable?: boolean
+    size?: 'small' | 'default' | 'large'
+    fetchFun: (params: SearchProps) => Promise<any>
+    labelParser?: (item: any) => string
+    injectProps?: InjectProps
+    max?: number
+    placeholder?: string
+}
+
+const props = withDefaults(defineProps<Props>(), {
+    optionKey: 'id',
+    keyword: 'keyword',
+    multiple: false,
+    disable: false,
+    size: 'default',
+    max: 100,
+    injectProps: () => ({}),
+    placeholder: '请选择'
+})
+
+const emit = defineEmits(['update:modelValue'])
+
+const value = computed({
+    get: () => props.modelValue,
+    set: (v) => {
+        emit('update:modelValue', v)
+    }
+})
+
+const inject = computed(() => props.injectProps)
+
+const lists = ref<any[]>([])
+
+const parseLabel = (item: any) => {
+    if (props.labelParser) {
+        return props.labelParser(item)
+    }
+
+    return String(item)
+}
+
+const loadSelect = async (k: string) => {
+    const data: SearchProps = { ...inject.value, [props.keyword]: k }
+    const res = await props.fetchFun(data)
+    lists.value = res.lists.slice(0, Math.min(props.max, 100))
 }
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
