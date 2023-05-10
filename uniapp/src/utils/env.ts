cd2c828e7a702f250fe33da3d243ffc6cd039cd7
Tenko_ <admin@tenko.life>
2023-05-10 10:40:43 +0800

    style: ts-ignore

--- uniapp/src/utils/env.ts
+++ uniapp/src/utils/env.ts
@@ -2,6 +2,7 @@
  * @description: 开发模式
  */
 export function isDevMode(): boolean {
+    // @ts-ignore
     return import.meta.env.DEV
 }
 
@@ -9,6 +10,7 @@ export function isDevMode(): boolean {
  * @description: 生成模式
  */
 export function isProdMode(): boolean {
+    // @ts-ignore
     return import.meta.env.PROD
 }
 
@@ -16,9 +18,12 @@ export function isProdMode(): boolean {
  * 检查是否是微信小程序的开发版或体验版
  */
 export function isWechatDevMode(): boolean {
+    // @ts-ignore
     if (typeof wx !== 'undefined' && typeof wx.getAccountInfoSync === 'function') {
+        // @ts-ignore
         const env = wx.getAccountInfoSync()
 
+        // @ts-ignore
         return enc?.miniProgram?.envVersion === 'develop' || enc?.miniProgram?.envVersion === 'trial'
     }
 
