67f504edfc09505d2a2e2400cde1d2ca92e4c834
Tenko_ <admin@tenko.life>
2023-05-10 10:06:42 +0800

    feat: 新增微信小程序环境检查

--- uniapp/src/utils/env.ts
+++ uniapp/src/utils/env.ts
@@ -11,3 +11,16 @@ export function isDevMode(): boolean {
 export function isProdMode(): boolean {
     return import.meta.env.PROD
 }
+
+/**
+ * 检查是否是微信小程序的开发版或体验版
+ */
+export function isWechatDevMode(): boolean {
+    if (typeof wx !== 'undefined' && typeof wx.getAccountInfoSync === 'function') {
+        const env = wx.getAccountInfoSync()
+
+        return enc?.miniProgram?.envVersion === 'develop' || enc?.miniProgram?.envVersion === 'trial'
+    }
+
+    return false
+}
