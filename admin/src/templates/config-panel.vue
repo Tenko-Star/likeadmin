33120e1c354bf5e1cd2a0f679198d4ecd2ea06ed
Tenko <admin@tenko.life>
2023-11-27 10:48:13 +0800

    lint code

--- admin/src/templates/config-panel.vue
+++ admin/src/templates/config-panel.vue
@@ -1,50 +1,50 @@
 <script setup lang="ts">
-import {useConfig} from "@/hooks/useConfig";
+import { useConfig } from '@/hooks/useConfig'
 
 const [formData, save] = useConfig('config-group-key', {
-  key1: 'value1',
-  key2: 'value2',
-  key3: 'value3',
-  key4: 'value4',
+    key1: 'value1',
+    key2: 'value2',
+    key3: 'value3',
+    key4: 'value4'
 })
 
 const saveWithRefresh = () => {
-  save()
+    save()
 }
 
 const saveWithoutRefresh = () => {
-  save(false)
+    save(false)
 }
 </script>
 
 <template>
-  <div class="xl:flex">
-    <el-card class="!border-none flex-1 xl:mr-4 mb-4" shadow="never">
-      <template #header>
-        <span class="font-medium">配置1</span>
-      </template>
-      <el-form :model="formData" label-width="80px">
-        <el-form-item label="配置名称">
-          <el-input v-model="formData.key1"/>
-        </el-form-item>
-      </el-form>
+    <div class="xl:flex">
+        <el-card class="!border-none flex-1 xl:mr-4 mb-4" shadow="never">
+            <template #header>
+                <span class="font-medium">配置1</span>
+            </template>
+            <el-form :model="formData" label-width="80px">
+                <el-form-item label="配置名称">
+                    <el-input v-model="formData.key1" />
+                </el-form-item>
+            </el-form>
 
-      <editor class="mb-10" v-model="formData.key2" height="500"></editor>
-    </el-card>
-    <el-card class="!border-none flex-1 mb-4" shadow="never">
-      <template #header>
-        <span class="font-medium">配置2</span>
-      </template>
-      <el-form :model="formData" label-width="80px">
-        <el-form-item label="协议名称">
-          <el-input v-model="formData.key3"/>
-        </el-form-item>
-      </el-form>
+            <editor class="mb-10" v-model="formData.key2" height="500"></editor>
+        </el-card>
+        <el-card class="!border-none flex-1 mb-4" shadow="never">
+            <template #header>
+                <span class="font-medium">配置2</span>
+            </template>
+            <el-form :model="formData" label-width="80px">
+                <el-form-item label="协议名称">
+                    <el-input v-model="formData.key3" />
+                </el-form-item>
+            </el-form>
 
-      <editor class="mb-10" v-model="formData.key4" height="500"></editor>
-    </el-card>
-  </div>
-  <footer-btns v-perms="['setting.web.web_setting/setAgreement']">
-    <el-button type="primary" @click="save">保存</el-button>
-  </footer-btns>
+            <editor class="mb-10" v-model="formData.key4" height="500"></editor>
+        </el-card>
+    </div>
+    <footer-btns v-perms="['setting.web.web_setting/setAgreement']">
+        <el-button type="primary" @click="save">保存</el-button>
+    </footer-btns>
 </template>
--- admin/src/templates/config-popup.vue
+++ admin/src/templates/config-popup.vue
@@ -1,17 +1,17 @@
 <script setup lang="ts">
-import {useConfig} from "@/hooks/useConfig"
-import {defineEmits, shallowRef} from 'vue'
-import type {FormInstance, FormRules} from "element-plus";
-import Popup from "@/components/popup/index.vue";
+import { useConfig } from '@/hooks/useConfig'
+import { defineEmits, shallowRef } from 'vue'
+import type { FormInstance, FormRules } from 'element-plus'
+import Popup from '@/components/popup/index.vue'
 import MaterialPicker from '@/components/material/picker.vue'
 
 const emit = defineEmits(['success', 'close'])
 const formRef = shallowRef<FormInstance>()
 const popupRef = shallowRef<InstanceType<typeof Popup>>()
 const [formData, save] = useConfig('config-group-key', {
-  key1: 'value1',
-  key2: 'value2',
-  key3: 'value3',
+    key1: 'value1',
+    key2: 'value2',
+    key3: 'value3'
 })
 
 // 表单验证
@@ -19,55 +19,50 @@ const formRules = reactive<FormRules>({})
 
 // 提交按钮
 const handleSubmit = async () => {
-  await formRef.value?.validate()
-  save()
-  popupRef.value?.close()
-  emit('success')
+    await formRef.value?.validate()
+    save()
+    popupRef.value?.close()
+    emit('success')
 }
 
 //打开弹窗
 const open = () => {
-  popupRef.value?.open()
+    popupRef.value?.open()
 }
 
 // 关闭回调
 const handleClose = () => {
-  emit('close')
+    emit('close')
 }
 
-
-
 defineExpose({
-  open
+    open
 })
 </script>
 
 <template>
-  <div class="config-popup">
-    <popup
-        ref="popupRef"
-        title="配置"
-        :async="true"
-        width="550px"
-        @confirm="handleSubmit"
-        @close="handleClose"
-    >
-      <el-form ref="formRef" :model="formData" label-width="90px" :rules="formRules">
-        <el-form-item label="配置1" prop="team_name">
-          <el-input v-model="formData.key1" clearable placeholder="" />
-        </el-form-item>
-        <el-form-item label="配置2" prop="slogan">
-          <el-input v-model="formData.key2" clearable placeholder="" />
-        </el-form-item>
-        <el-form-item label="配置3" prop="avatar">
-          <material-picker v-model="formData.key3" />
-        </el-form-item>
-
-      </el-form>
-    </popup>
-  </div>
+    <div class="config-popup">
+        <popup
+            ref="popupRef"
+            title="配置"
+            :async="true"
+            width="550px"
+            @confirm="handleSubmit"
+            @close="handleClose"
+        >
+            <el-form ref="formRef" :model="formData" label-width="90px" :rules="formRules">
+                <el-form-item label="配置1" prop="team_name">
+                    <el-input v-model="formData.key1" clearable placeholder="" />
+                </el-form-item>
+                <el-form-item label="配置2" prop="slogan">
+                    <el-input v-model="formData.key2" clearable placeholder="" />
+                </el-form-item>
+                <el-form-item label="配置3" prop="avatar">
+                    <material-picker v-model="formData.key3" />
+                </el-form-item>
+            </el-form>
+        </popup>
+    </div>
 </template>
 
-<style scoped lang="scss">
-
-</style>
+<style scoped lang="scss"></style>
